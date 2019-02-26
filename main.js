adf42adfebc0699eea93baa04280ed1f9d8af1e9
Mehulkumar <m3hul.g4m3r@gmail.com>
2019-02-26 20:49:49 +0530

    Simple todo graphql schema

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const fs = require('fs')
+
+// Parse GraphQL Schema
+const RawSchema = fs.readFileSync('todo.gql', { encoding: 'utf8' })
+
+// SERVER
 // Define types
 // Define fields on those types
 // Define resolvers
 
+// CLIENT-REQUEST
 // Validation
 // Execution
 // Introspection
