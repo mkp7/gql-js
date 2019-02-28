6af87503658f8d67ffbfa758b869de53c66883e3
Mehulkumar <m3hul.g4m3r@gmail.com>
2019-02-28 22:46:53 +0530

    Implementation of SchemaDefinition parser

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const objectTypeParser = inp => {
   const name = match[1]
   const fields = []
 
-  expr = /^\s*(\w+)\s*:\s*(String|Int)(!?)/
+  expr = /^\s*(\w+)\s*:\s*(\w+)(!?)/
   match = expr.exec(remInp)
 
   while (match !== null) {
@@ -47,6 +47,44 @@ const objectTypeParser = inp => {
   }, remInp.slice(match[0].length)]
 }
 
+const schemaDefinitionParser = inp => {
+  let expr = /^\s*schema\s+\{/
+  let match = expr.exec(inp)
+
+  if (match === null) {
+    return null
+  }
+
+  let remInp = inp.slice(match[0].length)
+  const operations = []
+
+  expr = /^\s*(\w+)\s*:\s*(\w+)/
+  match = expr.exec(remInp)
+
+  while (match !== null) {
+    operations.push({
+      name: match[1],
+      type: match[2]
+    })
+    remInp = remInp.slice(match[0].length)
+    match = expr.exec(remInp)
+  }
+
+  expr = /^\s*\}/
+  match = expr.exec(remInp)
+
+  if (match === null) {
+    return null
+  }
+
+  return [{
+    name: 'schema',
+    type: 'Schema',
+    operations: operations
+  }, remInp.slice(match[0].length)]
+}
+
+//
 const SchemaParser = inp => {
   const Schema = {}
   let remInp
@@ -58,8 +96,14 @@ const SchemaParser = inp => {
     match = objectTypeParser(remInp)
   }
 
-  console.log(Schema)
-  console.log(remInp)
+  match = schemaDefinitionParser(remInp)
+
+  if (match === null) {
+    throw new Error('schema definition not found')
+  }
+
+  console.log(Schema, match[0])
+  console.log(match[1])
 }
 
 SchemaParser(RawSchema)
