9b33002c7839af951462b56610328615535c34a5
Mehulkumar <m3hul.g4m3r@gmail.com>
2019-03-04 20:58:55 +0530

    fix schema def

--- test.js
+++ test.js
@@ -13,28 +13,27 @@ app.static('/public')
 
 // GraphQL config
 // Parse GraphQL Schema
-// const RawSchema = fs.readFileSync('schema.graphql', { encoding: 'utf8' })
-// const Schema = GraphQL.SchemaParser(RawSchema)
+const RawSchema = fs.readFileSync('schema.graphql', { encoding: 'utf8' })
+const Schema = GraphQL.SchemaParser(RawSchema)
+console.log(Schema)
 
-// console.log(Schema)
-
-// app.get('/', (req, res) => {
-//   res.body = 'Hello World, from GeekSkool.'
-
-//   return res
-// })
-
-// app.post('/graphql-api', (req, res) => {
-//   console.log(req.body)
-
-//   res.body = 'pong'
-
-//   return res
-// })
-
-// app.listen(3100, options)
-
+// Test GraphQL OperationDefinition parser
 const RawOperation = fs.readFileSync('test_query.txt', { encoding: 'utf8' })
 const Operation = GraphQL.operationDefinitionParser(RawOperation)
-
 console.log(Operation)
+
+app.get('/', (req, res) => {
+  res.body = 'Hello World, from GeekSkool.'
+
+  return res
+})
+
+app.post('/graphql-api', (req, res) => {
+  console.log(req.body.toString('utf8'))
+
+  res.body = 'pong'
+
+  return res
+})
+
+app.listen(3100, options)
