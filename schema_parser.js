e58dbcf8ed300e42d9320b33ee8404fbb634f19d
Mehulkumar <m3hul.g4m3r@gmail.com>
2019-03-04 08:56:38 +0530

    Implemented simple SchemaDefinition parser

---
+++ schema_parser.js
@@ -0,0 +1,102 @@
+// Scalar Types
+const ScalarTypes = ['String']
+
+// Object Type Parser
+// ^\s*type\s+(\w+)\s+\{(\s*(\w+)\s*:\s*(String|Int)!?)+\s*\}
+const objectTypeParser = inp => {
+  let expr = /^\s*type\s+(\w+)\s+\{/
+  let match = expr.exec(inp)
+
+  if (match === null) {
+    return null
+  }
+
+  let remInp = inp.slice(match[0].length)
+  const name = match[1]
+  const fields = []
+
+  expr = /^\s*(\w+)\s*:\s*(\w+)(!?)/
+  match = expr.exec(remInp)
+
+  while (match !== null) {
+    fields.push({
+      name: match[1],
+      type: match[2],
+      requried: match[3] === '!'
+    })
+    remInp = remInp.slice(match[0].length)
+    match = expr.exec(remInp)
+  }
+
+  expr = /^\s*\}/
+  match = expr.exec(remInp)
+
+  if (match === null) {
+    return null
+  }
+
+  return [{
+    name: name,
+    type: 'Object',
+    fields: fields
+  }, remInp.slice(match[0].length)]
+}
+
+const schemaDefinitionParser = inp => {
+  let expr = /^\s*schema\s+\{/
+  let match = expr.exec(inp)
+
+  if (match === null) {
+    return null
+  }
+
+  let remInp = inp.slice(match[0].length)
+  const operations = {}
+
+  expr = /^\s*(\w+)\s*:\s*(\w+)/
+  match = expr.exec(remInp)
+
+  while (match !== null) {
+    operations[match[1]] = match[2]
+    remInp = remInp.slice(match[0].length)
+    match = expr.exec(remInp)
+  }
+
+  expr = /^\s*\}/
+  match = expr.exec(remInp)
+
+  if (match === null) {
+    return null
+  }
+
+  return [{
+    name: 'schema',
+    type: 'Schema',
+    operations: operations
+  }, remInp.slice(match[0].length)]
+}
+
+//
+const SchemaParser = inp => {
+  const Schema = {}
+  let remInp
+  let match = objectTypeParser(inp)
+
+  while (match !== null) {
+    Schema[match[0].name] = match[0]
+    remInp = match[1]
+    match = objectTypeParser(remInp)
+  }
+
+  match = schemaDefinitionParser(remInp)
+
+  if (match === null) {
+    throw new Error('schema definition not found')
+  }
+
+  Schema['schema'] = match[0]
+
+  return Schema
+}
+
+module.exports = SchemaParser
---
+++ test.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const Server = require('../turbo-http2')
+const GraphQL = require('./main')
+
+const options = {
+  key: fs.readFileSync('../localhost-key.pem'),
+  cert: fs.readFileSync('../localhost.pem')
+}
+
+const app = new Server()
+
+app.static('/public')
+
+// GraphQL config
+// Parse GraphQL Schema
+const RawSchema = fs.readFileSync('schema.graphql', { encoding: 'utf8' })
+const Schema = GraphQL.SchemaParser(RawSchema)
+
+console.log(Schema)
+
+app.get('/', (req, res) => {
+  res.body = 'Hello World, from GeekSkool.'
+
+  return res
+})
+
+app.post('/graphql-api', (req, res) => {
+  console.log(req.body)
+
+  res.body = 'pong'
+
+  return res
+})
+
+app.listen(3100, options)
